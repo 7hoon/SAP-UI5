40a410d747292f7aa5242adf85b65706852a1fc4
chihoon <clgns7985@naver.com>
2023-09-13 14:50:52 +0900

    230913 v2

--- odatacrud_b05/webapp/controller/Main.controller.js
+++ odatacrud_b05/webapp/controller/Main.controller.js
@@ -9,6 +9,23 @@ sap.ui.define([
 
         return Controller.extend("odatacrudb05.controller.Main", {
             onInit: function () {
+                var oModel = this.getOwnerComponent().getModel();             // manifest 에 등록된 component 단의 기본 데이터를 가져옴 
+                
+                // read   = GET 요청
+                // create = POST 요청
+                // update = PUT 요청
+                // remove = DELETE 요청 
+            },
+            onRead: function () {
+
+            },
+            onCreate: function () {
+
+            },
+            onUpdate: function () {
+
+            },
+            onDelete: function () {
 
             }
         });
