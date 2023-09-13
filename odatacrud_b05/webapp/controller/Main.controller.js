be6afb22844219808b0f8668487cdc8d93ae3559
chihoon <clgns7985@naver.com>
2023-09-13 18:30:33 +0900

    230913

--- odatacrud_b05/webapp/controller/Main.controller.js
+++ odatacrud_b05/webapp/controller/Main.controller.js
@@ -18,6 +18,8 @@ sap.ui.define(
         // JSONModel 이름 : view
         this.getView().setModel(new JSONModel(), "view");
       },
+
+      // GET : /EntitySetName('keyValue')
       onRead: function () {
         var oJSONModel = this.getView().getModel("view"); // JSONModel
         var oItem = this.byId("idMemberSetTable")
@@ -44,21 +46,75 @@ sap.ui.define(
           },
         });
       },
+
+      // POST : /EntitySetName + BODY정보(JSON Data)
       onCreate: function () {
         var oDataModel = this.getOwnerComponent().getModel();
         var oJSONModel = this.getView().getModel("view");
-        var oBody = oJSONModel.getData();  // 생성할 데이터 구성
-        // var oBody = oJSONModel.getProperty("/"); // 생성할 데이터 구성
-        debugger;
+        
+        
+        var oBody = oJSONModel.getData(); // 생성할 데이터 구성
+        var oBody = oJSONModel.getProperty("/"); // 생성할 데이터 구성
+     
         oDataModel.create("/MemberSet", oBody, {
           success: function (oReturn) {
-            debugger;
+            sap.m.MessageToast.show("데이터 생성 완료");
+          },
+          error: function (oError) {},
+        });
+        // for (let i = 30; i < 41; i++) {
+        //     var oInput = this.byId("idInput");
+        //     oInput.setValue(i);
+        //     var oDataModel = this.getOwnerComponent().getModel();
+        //     var oJSONModel = this.getView().getModel("view");
+        //     var oBody = oJSONModel.getData();
+        //     oDataModel.create("/MemberSet", oBody, {success: function (oReturn) {
+        //             sap.m.MessageToast.show("데이터 생성 완료");
+        //           }
+        //       });
+        // }
+      },
+
+      // PUT : /EntitySetName('keyValue') + Body
+      onUpdate: function () {
+        var oDataModel = this.getOwnerComponent().getModel();
+        var oJSONModel = this.getView().getModel("view");
+        var oBody = oJSONModel.getData(); // 생성할 데이터 구성
+
+        // 아래와 같이 별도 구성해도 됨.
+        // var obj = {
+        //   MB_NM: oBody.MB_NM,
+        //   TELNO: oBody.TELNO,
+        //   EMAIL: oBody.EMAIL,
+        //   MB_ST: 'A'
+        // };
+
+        oDataModel.update(`/MemberSet('${oBody.MB_ID}')`, oBody, {
+          success: function (oReturn) {
+            sap.m.MessageToast.show("데이터 변경 완료");
           },
           error: function (oError) {},
         });
       },
-      onUpdate: function () {},
-      onDelete: function () {},
+
+      // DELETE : /EntitySetName('keyValue')
+      onDelete: function () {
+        var oDataModel = this.getView().getModel();
+        var oTable = this.byId("idMemberSetTable");
+        var sValue = oTable.getSelectedContexts()[0].getObject().MB_ID;
+        
+        var sPath = oDataModel.createKey("/MemberSet", {
+            MB_ID : sValue
+        });
+
+        oDataModel.remove(sPath,{
+            success: function (oReturn) {
+                sap.m.MessageToast.show("데이터가 삭제되었습니다.");
+                oDataModel.refresh(true);   // optional) 모델 리프레시
+              },
+              error: function (oError) {}
+        })
+      },
     });
   }
 );
