7045d688a9d2e55e1160a645d7a752d1326764a4
chihoon <clgns7985@naver.com>
2023-09-19 12:26:25 +0900

    230919 Odata 두개 붙이기

--- zproject_b05_09/webapp/controller/Main.controller.js
+++ zproject_b05_09/webapp/controller/Main.controller.js
@@ -1,15 +1,38 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Fragment) {
         "use strict";
 
         return Controller.extend("zprojectb0509.controller.Main", {
             onInit: function () {
 
-            }
+            },
+            onValueHelp: function () {
+                var oDialog = sap.ui.getCore().byId("idDialog");
+                var oModel = this.getView().getModel('scarr');
+                if (oDialog) {
+                  oDialog.open();
+                } else {
+                  Fragment.load({
+                    name: "zprojectb0509.view.fragment.Scarr",
+                    type: "XML",
+                    controller: this,
+                  }).then(function (oDialog) {
+                    oDialog.setModel(oModel, 'scarr');
+                    oDialog.open();
+                  });
+                }
+              },
+              onClose: function (oEvent) {
+                var oButton = oEvent.getSource();
+                var oDialog = oButton.getEventingParent();
+        
+                oDialog.close();
+              }
         });
     });
