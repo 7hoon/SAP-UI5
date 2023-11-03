25dc33f6ad22184d9f40a7ab2fbae4e935dc6ca4
chihoon <clgns7985@naver.com>
2023-11-03 14:48:07 +0900

    231103_v1

---
+++ zproject_zbb_fi010/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("zprojectzbbfi010.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ zproject_zbb_fi010/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zproject_zbb_fi010/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
