c6a2b0a9559828f09fa4c28af4ed215cf463fb69
chihoon <clgns7985@naver.com>
2023-11-20 18:51:11 +0900

    231120 v1

---
+++ zbbproject_030/webapp/controller/Main.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment",
+    "sap/ui/model/Filter",
+  ],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, JSONModel, Fragment, Filter) {
+    "use strict";
+
+    return Controller.extend("zbbproject030.controller.Main", {
+      onInit: function () {
+      
+      },
+    });
+  }
+);
---
+++ zbbproject_030/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zbbproject_030/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
