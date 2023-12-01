f7ee4ab4f94f134fa7e3725f6d0d0de28c282aaf
chihoon <clgns7985@naver.com>
2023-12-01 16:02:01 +0900

    231201_v1

--- zbbproject_030/webapp/controller/Detail.controller.js
+++ zbbproject_030/webapp/controller/Detail.controller.js
@@ -20,18 +20,29 @@ sap.ui.define(
       _patternMatched: function (oEvent) {
         var oParam = oEvent.getParameters().arguments; // paramOrder
         var oModel = this.getView().getModel("ZBB_MMT010_SRV"); // OData Model
-
+        var oModel2 = this.getView().getModel("ZBB_MMT020_SRV"); // OData Model
         // this.getView().bindElemenrt(`/Order_Details(OrderID=${oParam.paramOder},ProductID=${oParam.param2})`); // ex) /Order_Details(OrderID=10248,ProductID=11)
-
-        var sPath = oModel.createKey("/ZBB_MMT010Set", {
+        var sPath = oModel.createKey("/ZBB_MMT010Set", {                   // 자재정보
           MatCd: oParam.paramMatCd,
         }); // '/Order_Details(OrderID=10248,ProductID=11)'
 
         oModel.read(sPath, {
           success: function (oReturn) {
-            debugger;
             var oModel = new sap.ui.model.json.JSONModel(oReturn);
             this.getView().setModel(oModel, "MatCdDetails");
+          }.bind(this),
+          error: function (oError) {},
+        }); // Odata모델을 Json 모델로 다루는법
+        debugger;
+        var sPath2 = oModel2.createKey("/zbb_mmt020Set", {                      // 공급업체
+          SuplId: oParam.param2,
+        }); // '/Order_Details(OrderID=10248,ProductID=11)'
+   
+        oModel2.read(sPath2, {
+          success: function (oReturn) {
+      
+            var oModel = new sap.ui.model.json.JSONModel(oReturn);
+            this.getView().setModel(oModel, "SuplIdDetails");
           }.bind(this),
           error: function (oError) {},
         }); // Odata모델을 Json 모델로 다루는법
